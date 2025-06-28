51c53714d72d18d6c245df2ca6d564912b7bb2a3
Alek H <alekh@debian.dolphin>
2025-06-27 19:01:15 -0500

    Turned it into a loop, to be improved on later

--- airreq.py
+++ airreq.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # TODO: Change this to be current location, add customizations
 # to the request s.t you can change radius of search
@@ -7,27 +8,37 @@ lon = -93.2144
 dist = 10
 
 url = f"https://api.adsb.lol/v2/closest/{lat}/{lon}/{dist}"
-response = requests.get(url)
+session = requests.Session()
 
-if response.status_code == 200:
-    data = response.json()
-    aircraft_list = data.get("ac", [])
+# TODO: Maybe set up signals instead of hard-exiting program after
+while True:
+    try:
+        response = session.get(url, timeout=5)
+        if response.status_code == 200:
+            data = response.json()
+            aircraft_list = data.get("ac", [])
+            if not aircraft_list:
+                print("No aircraft found.")
+            else:
+                for aircraft in aircraft_list:
+                    flight = aircraft.get("flight", "").strip()
+                    alt = aircraft.get("alt_baro", 0)
+                    speed = aircraft.get("gs", 0)
+                    model = aircraft.get("type", "Unknown")
+                    desc = aircraft.get("t", "Unknown")
+                    reg = aircraft.get("r", "N/A")
+                    lat = aircraft.get("lat", 0)
+                    lon = aircraft.get("lon", 0)
+                    # TODO: Given the flight number, run it through FlightStats and scrape
+                    # for information on route such as airport, dest, airline, delays, gate etc.
+                    print(f"Flight: {flight} | Reg: {reg}")
+                    print(f"Model: {desc} ({model}) | Altitude: {alt} ft | Speed: {speed} kt")
+                    print(f"Location: {lat}, {lon}\n")
+        else:
+            print(f"Error: ", response.status_code)
+    except Exception as e:
+        print("Error: ", e)
 
-    if not aircraft_list:
-        print("No aircraft found.")
-    else:
-        for aircraft in aircraft_list:
-            flight = aircraft.get("flight", "").strip()
-            alt = aircraft.get("alt_baro", 0)
-            speed = aircraft.get("gs", 0)
-            model = aircraft.get("type", "Unknown")
-            desc = aircraft.get("t", "Unknown")
-            reg = aircraft.get("r", "N/A")
-            lat = aircraft.get("lat", 0)
-            lon = aircraft.get("lon", 0)
-            # TODO: Given the flight number, run it through FlightStats and scrape
-            # for information on route such as airport, dest, airline, delays, gate etc.
-            print(f"Flight: {flight} | Reg: {reg}")
-            print(f"Model: {desc} ({model}) | Altitude: {alt} ft | Speed: {speed} kt")
-            print(f"Location: {lat}, {lon}\n")
+    time.sleep(5)
+
 
